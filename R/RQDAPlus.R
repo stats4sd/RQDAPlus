a14511334b76bbbf550501a90be414905cdf5bd6
sdumble1 <s.dumble@stats4sd.org>
2019-03-19 17:42:49 +0000

    Add files via upload

--- R/RQDAPlus.R
+++ R/RQDAPlus.R
@@ -22,6 +22,7 @@ ui <- fluidPage(
    sidebarLayout(
       sidebarPanel(
         selectizeInput("cases", "Cases", choices = "",multiple = TRUE),
+        selectInput("codcat","Selection of Codes by:",choices=c("Code","Category")),
         selectizeInput("codes", "Codes", choices = "",multiple = TRUE),
         selectizeInput("files", "Files", choices = "",multiple = TRUE),
         downloadButton("downloadData", "Download Selected Text as CSV"),
@@ -53,21 +54,67 @@ server <- function(input, output,session) {
   observe({
 
  fc<-dbGetQuery(con,"select name,id from freecode")
- fc2<-dbGetQuery(con,"select name,id from cases")
+
+ fc3<-dbGetQuery(con,"select name,catid from codecat")
     avail_codes<-fc$name[fc$id%in%unique(dbGetQuery(con,"select cid from coding")$cid)]
-      avail_cases<-fc2$name[fc$id%in%unique(dbGetQuery(con,"select caseid from caselinkage")$caseid)]
-      avail_cases<-sort(avail_cases)
+
+      avail_cats<-fc3$name[fc3$catid%in%unique(dbGetQuery(con,"select catid from treecode")$catid)]
+
       avail_codes<-sort(avail_codes)
+      avail_cats<-sort(avail_cats)
+
+
+    if(input$codcat=="Code"){
+    updateSelectInput(session, "codes", choices = avail_codes)
+
+
+
+      }
+    else{
+    updateSelectInput(session, "codes", choices = avail_cats)
+     }
+
+
+  })
+
+  observe({
+    fc<-dbGetQuery(con,"select name,id from freecode")
+    fc2<-dbGetQuery(con,"select name,id from cases")
+
+
+    avail_cases<-fc2$name[fc2$id%in%unique(dbGetQuery(con,"select caseid from caselinkage")$caseid)]
+
+    avail_cases<-sort(avail_cases)
+
     avail_files<-dbGetQuery(con,"select name from source")
 
-    updateSelectInput(session, "codes", choices = avail_codes)
     updateSelectInput(session, "cases", choices = avail_cases)
     updateSelectInput(session, "files", choices = avail_files)
   })
 
+
+
+
   output$Output0 <- renderDataTable({
+    if(input$codcat=="Code"){
+      codes<-input$codes
+    }
+    else{
+      if(is.null(input$codes)){
+        codes<-NULL
+      }
+      else{
+        codes1<-dbGetQuery(con,paste("select cid from treecode where catid IN",
+                                     paste("('",paste(fc3$catid[fc3$name%in%input$codes],collapse="','"),"')",sep="")))$cid
+        codes<-dbGetQuery(con,paste("select name from freecode where id IN",
+                                    paste("('",paste(codes1,collapse="','"),"')",sep="")))$name
+      }
+    }
+
+
+
     if(!is.null(input$cases)){
-      df<-codeIncase(input$cases,input$codes,output = "df",connection=connection,files = input$files)
+      df<-codeIncase(input$cases,codes,output = "df",connection=connection,files = input$files)
 
       if(ncol(df)==1){
         out<-df
@@ -82,8 +129,24 @@ server <- function(input, output,session) {
 
 
    output$Output1 <- renderDataTable({
+     if(input$codcat=="Code"){
+       codes<-input$codes
+     }
+     else{
+       if(is.null(input$codes)){
+         codes<-NULL
+       }
+       else{
+         codes1<-dbGetQuery(con,paste("select cid from treecode where catid IN",
+                                      paste("('",paste(fc3$catid[fc3$name%in%input$codes],collapse="','"),"')",sep="")))$cid
+         codes<-dbGetQuery(con,paste("select name from freecode where id IN",
+                                     paste("('",paste(codes1,collapse="','"),"')",sep="")))$name
+       }
+     }
+
+
      if(!is.null(input$cases)){
-     out<-codeIncase(input$cases,input$codes,output = "df",connection=connection,files = input$files)
+     out<-codeIncase(input$cases,codes,output = "df",connection=connection,files = input$files)
      if(ncol(out)==1){
        out<-out
      }
@@ -102,7 +165,24 @@ server <- function(input, output,session) {
        paste("Output",ceiling(round(runif(1,1,1000))), ".csv", sep = "")
      },
      content = function(file) {
-       write.csv(codeIncase(input$cases,input$codes,output = "df",connection=connection,files = input$files)[,c(3,4,2,5)],
+
+       if(input$codcat=="Code"){
+         codes<-input$codes
+       }
+       else{
+         if(is.null(input$codes)){
+           codes<-NULL
+         }
+         else{
+           codes1<-dbGetQuery(con,paste("select cid from treecode where catid IN",
+                                        paste("('",paste(fc3$catid[fc3$name%in%input$codes],collapse="','"),"')",sep="")))$cid
+           codes<-dbGetQuery(con,paste("select name from freecode where id IN",
+                                       paste("('",paste(codes1,collapse="','"),"')",sep="")))$name
+         }
+       }
+
+
+       write.csv(codeIncase(input$cases,codes,output = "df",connection=connection,files = input$files)[,c(3,4,2,5)],
                  file, row.names = FALSE)
      }
    )
@@ -112,7 +192,23 @@ server <- function(input, output,session) {
        paste("RQDAOutput",ceiling(round(runif(1,1,1000))), ".html", sep = "")
      },
      content = function(file) {
-       out<-codeIncase(input$cases,input$codes,output = "df",connection=connection,files = input$files)
+       if(input$codcat=="Code"){
+         codes<-input$codes
+       }
+       else{
+         if(is.null(input$codes)){
+           codes<-NULL
+         }
+         else{
+           codes1<-dbGetQuery(con,paste("select cid from treecode where catid IN",
+                                        paste("('",paste(fc3$catid[fc3$name%in%input$codes],collapse="','"),"')",sep="")))$cid
+           codes<-dbGetQuery(con,paste("select name from freecode where id IN",
+                                       paste("('",paste(codes1,collapse="','"),"')",sep="")))$name
+         }
+       }
+
+
+       out<-codeIncase(input$cases,codes,output = "df",connection=connection,files = input$files)
          out$seltext<-paste("<p>",gsub("[\n]","<br></br>",out$seltext),"</p>")
        export(out[,c(3,4,2,5)],file, row.names = FALSE)
      }
@@ -127,11 +223,24 @@ server <- function(input, output,session) {
 
 tab<-doc_adjacency(connection=connection,type="unit")
 
+if(input$codcat=="Code"){
+  codes<-input$codes
+}
+else{
+  if(is.null(input$codes)){
+    codes<-NULL
+  }
+  else{
+    codes1<-dbGetQuery(con,paste("select cid from treecode where catid IN",
+                                 paste("('",paste(fc3$catid[fc3$name%in%input$codes],collapse="','"),"')",sep="")))$cid
+    codes<-dbGetQuery(con,paste("select name from freecode where id IN",
+                                paste("('",paste(codes1,collapse="','"),"')",sep="")))$name
+  }
+}
 
 
-
-if(length(unique(c(input$cases,input$codes)))>1){
-     tab<-tab[colnames(tab)%in%unique(c(input$cases,input$codes)),colnames(tab)%in%unique(c(input$cases,input$codes))]
+if(length(unique(c(input$cases,codes)))>1){
+     tab<-tab[colnames(tab)%in%unique(c(input$cases,codes)),colnames(tab)%in%unique(c(input$cases,codes))]
 }
         tab<-data.frame(Code=rownames(tab),tab)
      rownames(tab)<-NULL
@@ -141,13 +250,42 @@ if(length(unique(c(input$cases,input$codes)))>1){
 
    output$Output3 <- renderPlot(width=800,height=800,{
 
+     if(input$codcat=="Code"){
+       codes<-input$codes
+     }
+     else{
+       if(is.null(input$codes)){
+         codes<-NULL
+       }
+       else{
+         codes1<-dbGetQuery(con,paste("select cid from treecode where catid IN",
+                                      paste("('",paste(fc3$catid[fc3$name%in%input$codes],collapse="','"),"')",sep="")))$cid
+         codes<-dbGetQuery(con,paste("select name from freecode where id IN",
+                                     paste("('",paste(codes1,collapse="','"),"')",sep="")))$name
+       }
+     }
+
+
   RQDAnetwork(doc_adjacency(connection=connection,type="unit",
-                            files = input$files,code=input$code,case=input$case),group=input$group)
+                            files = input$files,code=codes,case=input$case),group=input$group)
 
 
    })
    output$cloud1 <- renderWordcloud2({
-
+     if(input$codcat=="Code"){
+       codes<-input$codes
+     }
+     else{
+       if(is.null(input$codes)){
+         codes<-NULL
+       }
+       else{
+         codes1<-dbGetQuery(con,paste("select cid from treecode where catid IN",
+                                      paste("('",paste(fc3$catid[fc3$name%in%input$codes],collapse="','"),"')",sep="")))$cid
+         codes<-dbGetQuery(con,paste("select name from freecode where id IN",
+                                     paste("('",paste(codes1,collapse="','"),"')",sep="")))$name
+       }
+     }
 
      if(is.null(input$cases)){
        if(is.null(input$files)){
@@ -157,20 +295,20 @@ if(length(unique(c(input$cases,input$codes)))>1){
          filelist<-input$files
        }
 
-       if(is.null(input$files)&is.null(input$codes)){
+       if(is.null(input$files)&is.null(codes)){
          txt<-dbGetQuery(con,"select file from source")$file
        }
-       if((!is.null(input$files))&is.null(input$codes)){
+       if((!is.null(input$files))&is.null(codes)){
 
          txt<-dbGetQuery(con,paste("select file from source where name IN",
                                    paste("('",paste(filelist,collapse="','"),"')",sep=""),sep=""))$file
        }
 
-       if(!is.null(input$codes)){
+       if(!is.null(codes)){
          sources<-dbGetQuery(con,paste("select id from source where name IN",
                                       paste("('",paste(filelist,collapse="','"),"')",sep=""),sep=""))
          codes<-dbGetQuery(con,paste("select id from freecode where name IN",
-                                       paste("('",paste(input$codes,collapse="','"),"')",sep=""),sep=""))
+                                       paste("('",paste(codes,collapse="','"),"')",sep=""),sep=""))
          txt<-dbGetQuery(con,paste("select seltext from coding where fid IN",
                          paste("('",paste(sources$id,collapse="','"),"')",sep=""),
                          " and cid IN",
@@ -179,7 +317,7 @@ if(length(unique(c(input$cases,input$codes)))>1){
      }
 
      if(!is.null(input$cases)){
-            txt<-codeIncase(input$cases,input$codes,output = "df",connection=connection,files=input$files)$text
+            txt<-codeIncase(input$cases,codes,output = "df",connection=connection,files=input$files)$text
        }
 if(length(txt)>0){
      t1<-removePunctuation(tolower(unlist(str_split(stripWhitespace(txt)," "))))
