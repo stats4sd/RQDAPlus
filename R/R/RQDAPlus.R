3daca0fcb7167873c4f4c70f9b94ce0681e7064c
sdumble1 <s.dumble@stats4sd.org>
2019-04-03 16:35:22 +0100

    Add files via upload

---
+++ R/R/RQDAPlus.R
@@ -0,0 +1,399 @@
+#' RQDAPlus
+#'
+#' Shiny interface to additional features using an RQDA project file. Currently
+#' implemented options for i) subsetting coded extracts to within certain codes
+#' WordClouds of extracts, or overall text, or individual files Adjacency
+#' matrices based on code occurence at within document and within case level
+#' Network analysis based on code co-occurence at either within document or
+#' within case level, with optional additional grouping algorithm
+#' @param connection .rqda project file
+#' @keywords RQDA Shiny Network Cloud
+#' @export
+RQDAPlus<-function(connection=NULL){
+  require(shiny)
+  require(DT)
+  require(wordcloud2)
+  require(tm)
+  require(stringr)
+  require(DBI)
+  require(rio)
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+   # Application title
+   titlePanel("RQDA Plus"),
+
+   # Sidebar with a slider input for number of bins
+   sidebarLayout(
+      sidebarPanel(
+        selectizeInput("cases", "Cases", choices = "",multiple = TRUE),
+        selectInput("codcat","Selection of Codes by:",choices=c("Code","Category")),
+        selectizeInput("codes", "Codes", choices = "",multiple = TRUE),
+        selectizeInput("files", "Files", choices = "",multiple = TRUE),
+        downloadButton("downloadData", "Download Selected Text as CSV"),
+        downloadButton("downloadData2", "Download Selected Text as HTML")
+      ),
+
+      # Show a plot of the generated distribution
+      mainPanel(width = 6,
+                tabsetPanel(type = "tabs",
+    tabPanel("Extracts from Codes Within Cases",
+             htmlOutput("Title1"),
+             dataTableOutput("Output0"),
+             dataTableOutput("Output1")),
+    tabPanel("WordCloud",htmlOutput("Title4"),wordcloud2Output("cloud1")),
+    tabPanel("Adjacency Matrix",htmlOutput("Title2"),
+             selectInput("AdjType","Adjacency of Codes Within:",choices=c("Document","Case")),
+             dataTableOutput("Output2")),
+    tabPanel("Network Analysis",htmlOutput("Title3"),
+             selectInput("NetType","Adjacency of Codes Within:",choices=c("Document","Case")),
+             checkboxInput("group", "Group Nodes?", value=FALSE),
+             numericInput("max", "Maximum Nodes to Include", value=75),
+             plotOutput("Output3"))
+   ,tags$style("#Title1{color: black;
+                                 font-size: 22px;
+                        font-style: bold;}"),
+   tags$style("#Title2{color: black;
+                                 font-size: 22px;
+               font-style: bold;}"),
+   tags$style("#Title3{color: black;
+                                 font-size: 22px;
+                        font-style: bold;}"),
+   tags$style("#Title4{color: black;
+                                 font-size: 22px;
+                        font-style: bold;}")
+      )
+      )
+   )
+
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output,session) {
+
+
+    con <- dbConnect(RSQLite::SQLite(), connection)
+
+
+
+  observe({
+
+ fc<-dbGetQuery(con,"select name,id from freecode")
+
+ fc3<-dbGetQuery(con,"select name,catid from codecat")
+    avail_codes<-fc$name[fc$id%in%unique(dbGetQuery(con,"select cid from coding")$cid)]
+
+      avail_cats<-fc3$name[fc3$catid%in%unique(dbGetQuery(con,"select catid from treecode")$catid)]
+
+      avail_codes<-sort(avail_codes)
+      avail_cats<-sort(avail_cats)
+
+
+    if(input$codcat=="Code"){
+    updateSelectInput(session, "codes", choices = avail_codes)
+
+
+
+      }
+      if(input$codcat=="Category"){
+    updateSelectInput(session, "codes", choices = avail_cats)
+     }
+
+
+  })
+
+  observe({
+    fc<-dbGetQuery(con,"select name,id from freecode")
+    fc2<-dbGetQuery(con,"select name,id from cases")
+
+
+    avail_cases<-fc2$name[fc2$id%in%unique(dbGetQuery(con,"select caseid from caselinkage")$caseid)]
+
+    avail_cases<-sort(avail_cases)
+
+    avail_files<-dbGetQuery(con,"select name from source")
+
+    updateSelectInput(session, "cases", choices = avail_cases)
+    updateSelectInput(session, "files", choices = avail_files)
+  })
+
+
+
+  output$Title1<-renderText({
+    if(input$codcat=="Code"){x=paste("Frequency of code(s)",paste(input$codes,collapse=", "),
+                                     "found within case(s)",paste(input$cases,collapse=", "))}
+    else{x=paste("Frequency of code category(s)",paste(input$codes,collapse=", "),
+                                     "found within case(s)",paste(input$cases,collapse=", "))}
+    x
+  })
+
+  output$Title2<-renderText({
+   paste("Adjacency matrix showing code co-occurence within each",tolower(input$AdjType),sep=" ")
+     })
+
+  output$Title3<-renderText({
+   if(input$group==FALSE){y=paste("Network analysis based on code co-occurence within each",tolower(input$NetType),sep=" ")}
+    if(input$group==TRUE){y=paste("Network analysis based on code co-occurence within each",tolower(input$NetType),
+                                "with clustering of related codes",sep=" ")}
+    y
+  })
+
+  output$Title4<-renderText({
+    if(input$codcat=="Code"){x=paste("Wordcloud of entries coded",paste(input$codes,collapse=", "),
+                                     "found within case(s)",paste(input$cases,collapse=", "))}
+    else{x=paste("Wordcloud of entries coded within the category(s) of",paste(input$codes,collapse=", "),
+                 "found within case(s)",paste(input$cases,collapse=", "))}
+    x
+  })
+  output$Output0 <- renderDataTable({
+    if(input$codcat=="Code"){
+      codes<-input$codes
+    }
+    else{
+      if(is.null(input$codes)){
+        codes<-NULL
+      }
+      else{
+        fc3<-dbGetQuery(con,"select name,catid from codecat")
+        codes1<-dbGetQuery(con,paste("select cid from treecode where catid IN",
+                                     paste("('",paste(fc3$catid[fc3$name%in%input$codes],collapse="','"),"')",sep="")))$cid
+        codes<-dbGetQuery(con,paste("select name from freecode where id IN",
+                                    paste("('",paste(codes1,collapse="','"),"')",sep="")))$name
+      }
+    }
+
+
+
+    if(!is.null(input$cases)){
+      df<-codeIncase(input$cases,codes,output = "df",connection=connection,files = input$files)
+
+      if(ncol(df)==1){
+        out<-df
+      }
+      else{
+        out<-aggregate(position~code+Case,data=df,FUN=length)
+        colnames(out)[3]<-"Instances"
+      }
+      out
+    }
+      })
+
+
+   output$Output1 <- renderDataTable({
+     if(input$codcat=="Code"){
+       codes<-input$codes
+     }
+     else{
+       if(is.null(input$codes)){
+         codes<-NULL
+       }
+       else{
+         fc3<-dbGetQuery(con,"select name,catid from codecat")
+         codes1<-dbGetQuery(con,paste("select cid from treecode where catid IN",
+                                      paste("('",paste(fc3$catid[fc3$name%in%input$codes],collapse="','"),"')",sep="")))$cid
+         codes<-dbGetQuery(con,paste("select name from freecode where id IN",
+                                     paste("('",paste(codes1,collapse="','"),"')",sep="")))$name
+       }
+     }
+
+
+     if(!is.null(input$cases)){
+     out<-codeIncase(input$cases,codes,output = "df",connection=connection,files = input$files)
+     if(ncol(out)==1){
+       out<-out
+     }
+     else{
+       out<-out[,c(5,2,4,3)]
+     }
+     }
+     else{
+       out<-data.frame("Case"="No cases selected")
+     }
+     out
+   })
+
+   output$downloadData <- downloadHandler(
+     filename = function() {
+       paste("Output",ceiling(round(runif(1,1,1000))), ".csv", sep = "")
+     },
+     content = function(file) {
+
+       if(input$codcat=="Code"){
+         codes<-input$codes
+       }
+       else{
+         if(is.null(input$codes)){
+           codes<-NULL
+         }
+         else{
+           fc3<-dbGetQuery(con,"select name,catid from codecat")
+           codes1<-dbGetQuery(con,paste("select cid from treecode where catid IN",
+                                        paste("('",paste(fc3$catid[fc3$name%in%input$codes],collapse="','"),"')",sep="")))$cid
+           codes<-dbGetQuery(con,paste("select name from freecode where id IN",
+                                       paste("('",paste(codes1,collapse="','"),"')",sep="")))$name
+         }
+       }
+
+
+       write.csv(codeIncase(input$cases,codes,output = "df",connection=connection,files = input$files)[,c(3,4,2,5)],
+                 file, row.names = FALSE)
+     }
+   )
+
+   output$downloadData2  <- downloadHandler(
+     filename = function() {
+       paste("RQDAOutput",ceiling(round(runif(1,1,1000))), ".html", sep = "")
+     },
+     content = function(file) {
+       if(input$codcat=="Code"){
+         codes<-input$codes
+       }
+       else{
+         if(is.null(input$codes)){
+           codes<-NULL
+         }
+         else{
+           fc3<-dbGetQuery(con,"select name,catid from codecat")
+           codes1<-dbGetQuery(con,paste("select cid from treecode where catid IN",
+                                        paste("('",paste(fc3$catid[fc3$name%in%input$codes],collapse="','"),"')",sep="")))$cid
+           codes<-dbGetQuery(con,paste("select name from freecode where id IN",
+                                       paste("('",paste(codes1,collapse="','"),"')",sep="")))$name
+         }
+       }
+
+
+       out<-codeIncase(input$cases,codes,output = "df",connection=connection,files = input$files)
+         out$seltext<-paste("<p>",gsub("[\n]","<br></br>",out$seltext),"</p>")
+       export(out[,c(3,4,2,5)],file, row.names = FALSE)
+     }
+   )
+
+
+
+
+
+   output$Output2 <- renderDataTable({
+
+
+tab<-doc_adjacency(connection=connection,type="unit",level=input$AdjType)
+
+if(input$codcat=="Code"){
+  codes<-input$codes
+}
+else{
+  if(is.null(input$codes)){
+    codes<-NULL
+  }
+  else{
+    fc3<-dbGetQuery(con,"select name,catid from codecat")
+    codes1<-dbGetQuery(con,paste("select cid from treecode where catid IN",
+                                 paste("('",paste(fc3$catid[fc3$name%in%input$codes],collapse="','"),"')",sep="")))$cid
+    codes<-dbGetQuery(con,paste("select name from freecode where id IN",
+                                paste("('",paste(codes1,collapse="','"),"')",sep="")))$name
+  }
+}
+
+
+if(length(unique(c(input$cases,codes)))>1){
+     tab<-tab[colnames(tab)%in%unique(c(input$cases,codes)),colnames(tab)%in%unique(c(input$cases,codes))]
+}
+        tab<-data.frame(Code=rownames(tab),tab)
+     rownames(tab)<-NULL
+   tab
+   })
+
+
+   output$Output3 <- renderPlot(width=800,height=800,{
+
+     if(input$codcat=="Code"){
+       codes<-input$codes
+     }
+     else{
+       if(is.null(input$codes)){
+         codes<-NULL
+       }
+       else{
+         fc3<-dbGetQuery(con,"select name,catid from codecat")
+         codes1<-dbGetQuery(con,paste("select cid from treecode where catid IN",
+                                      paste("('",paste(fc3$catid[fc3$name%in%input$codes],collapse="','"),"')",sep="")))$cid
+         codes<-dbGetQuery(con,paste("select name from freecode where id IN",
+                                     paste("('",paste(codes1,collapse="','"),"')",sep="")))$name
+       }
+     }
+
+
+  RQDAnetwork(doc_adjacency(connection=connection,type="unit",level=input$NetType),group=input$group,
+              maxnodes = input$max)
+
+
+   })
+   output$cloud1 <- renderWordcloud2({
+     if(input$codcat=="Code"){
+       codes<-input$codes
+     }
+     else{
+       if(is.null(input$codes)){
+         codes<-NULL
+       }
+       else{
+         fc3<-dbGetQuery(con,"select name,catid from codecat")
+         codes1<-dbGetQuery(con,paste("select cid from treecode where catid IN",
+                                      paste("('",paste(fc3$catid[fc3$name%in%input$codes],collapse="','"),"')",sep="")))$cid
+         codes<-dbGetQuery(con,paste("select name from freecode where id IN",
+                                     paste("('",paste(codes1,collapse="','"),"')",sep="")))$name
+       }
+     }
+
+     if(is.null(input$cases)){
+       if(is.null(input$files)){
+         filelist<-dbGetQuery(con,"select name from source")$name
+       }
+       else{
+         filelist<-input$files
+       }
+
+       if(is.null(input$files)&is.null(codes)){
+         txt<-dbGetQuery(con,"select file from source")$file
+       }
+       if((!is.null(input$files))&is.null(codes)){
+
+         txt<-dbGetQuery(con,paste("select file from source where name IN",
+                                   paste("('",paste(filelist,collapse="','"),"')",sep=""),sep=""))$file
+       }
+
+       if(!is.null(codes)){
+         sources<-dbGetQuery(con,paste("select id from source where name IN",
+                                      paste("('",paste(filelist,collapse="','"),"')",sep=""),sep=""))
+         codes<-dbGetQuery(con,paste("select id from freecode where name IN",
+                                       paste("('",paste(codes,collapse="','"),"')",sep=""),sep=""))
+         txt<-dbGetQuery(con,paste("select seltext from coding where fid IN",
+                         paste("('",paste(sources$id,collapse="','"),"')",sep=""),
+                         " and cid IN",
+                         paste("('",paste(codes$id,collapse="','"),"')",sep=""),sep=""))$seltext
+       }
+     }
+
+     if(!is.null(input$cases)){
+            txt<-codeIncase(input$cases,codes,output = "df",connection=connection,files=input$files)$text
+       }
+if(length(txt)>0){
+     t1<-removePunctuation(tolower(unlist(str_split(stripWhitespace(txt)," "))))
+     t1<-t1[!t1%in%stopwords("english")]
+     t2<-data.frame(table(t1))
+     t2<-slice(t2[rev(order(t2$Freq)),],1:250)
+
+     wordcloud2(t2,size=0.7)
+   }
+   })
+
+   session$onSessionEnded(function() {
+     dbDisconnect(con)
+     stopApp()
+   })
+
+   }
+
+# Run the application
+shinyApp(ui = ui, server = server,options =list(launch.browser = TRUE) )
+
+}
---
+++ R/R/codeIncase.R
@@ -0,0 +1,128 @@
+#' codeIncase
+#'
+#' Find overlap in highlighted text between "codes" and "cases"
+#' @param case vector of "cases"
+#' @param code vector of "codes" to search within the "cases"
+#' @param output output format df=data frame ; HTML = HTML file
+#' @param files vector of files to restrict search to
+#' @param connection .rqda project file
+#' @keywords RQDA Shiny Network Cloud
+#' @export
+
+codeIncase <-function(case,code=NULL,output="df",files=NULL,connection=NULL){
+  require(R2HTML)
+  require(purrr)
+  require(dplyr)
+  require(RSQLite)
+  require(rstudioapi)
+
+
+  #pick up link to SQL database
+  if(!is.null(connection)){
+    con <- dbConnect(RSQLite::SQLite(), connection)
+  }
+  else{
+    con <- .rqda$qdacon
+  }
+
+  #get matching cases
+  caseid<-dbGetQuery(con,paste("select name,id from cases where status=1 and name IN",
+                          paste("('",paste(case,collapse="','"),"')",sep=""),sep=""))
+
+  #give error if no cases found
+  if(any(!case%in%caseid$name)){
+    tmp<-case[!case%in%caseid$name]
+    stop(paste("No case '",paste(tmp,collapse="','"),"' found in database",sep=""))
+  }
+
+
+  pos<-dbGetQuery(con,paste("select fid,caseid,selfirst,selend from caselinkage where status=1 and caseid IN",
+                       paste("('",paste(caseid$id,collapse="','"),"')",sep="")
+                       ))
+
+
+  coding<-dbGetQuery(con,"select cid,fid,selfirst,selend from coding where status=1")
+  freecode<-dbGetQuery(con,"select name,id from freecode where status=1")
+
+  source<-dbGetQuery(con,"select name,id from source where status=1")
+  colnames(source)<-c("filename","fid")
+
+  pos$ID<-1:nrow(pos)
+  coding$ID<-1:nrow(coding)
+  coding<-inner_join(coding,source,by=c("fid"="fid"))
+
+  if(is.null(code)==FALSE){
+    codingid<-dbGetQuery(con,paste("select id,name from freecode where status=1 and name IN",
+                              paste("('",paste(code,collapse="','"),"')",sep=""),sep=""))
+
+    if(any(!code%in%codingid$name)){
+      tmp<-code[!code%in%codingid$name]
+      stop(paste("No code '",paste(tmp,collapse="','"),"' found in database",sep=""))
+    }
+
+
+    coding<-subset(coding,cid%in%as.integer(codingid$id))
+  }
+
+
+  if(is.null(files)==FALSE){
+    coding=subset(coding,filename%in%files)
+    if(nrow(coding)==0){
+      stop("No selected codes found in selected files")
+    }
+  }
+
+
+
+  outputlist<-map_df(pos$ID,function(x)
+    data.frame(subset(mutate(coding,Case=caseid$name[caseid$id==pos$caseid[pos$ID==x]],xID=x),
+                      fid==pos$fid[x]&selfirst>=pos$selfirst[x]&selend<=pos$selend[x])))
+
+  if(nrow(outputlist)==0){
+    return(data.frame(Case=(paste("No instances of '",code,"' found in '",case,"'",sep=""))))
+  }
+  outputlist$NewID<-1:nrow(outputlist)
+    outputtext<-map_df(outputlist$NewID,
+                     function(x)data.frame(y=x,seltext=dbGetQuery(con,paste("select seltext from coding where status=1 and cid=",
+                                                outputlist$cid[outputlist$NewID==x],
+                                                "AND fid=",outputlist$fid[outputlist$NewID==x],
+                                                "AND selfirst=",outputlist$selfirst[outputlist$NewID==x],
+                                                "AND selend=",outputlist$selend[outputlist$NewID==x]))$seltext))
+
+    outputtext<-outputtext[!duplicated(outputtext$y),]
+
+  list1<-outputlist %>% left_join(freecode,by=c("cid"="id"))%>%
+    select(selfirst,name,filename,Case,xID) %>% cbind(outputtext$seltext) %>% filter(is.na(name)==FALSE)
+  colnames(list1)<-c("position","code","filename","Case","ID","text")
+  list1<-list1[,c(1:4,6,5)]
+  list1$text<-trimws(list1$text)
+  if(output=="HTML"){
+    filename=paste("Output",ceiling(round(runif(1,1,1000))+as.numeric(Sys.time())/1000),sep="_")
+  HTMLStart(outdir=getwd(),filename =filename,
+            extension="html", echo=FALSE, HTMLframe=FALSE)
+  for(j in 1:length(case)){
+
+    tmp<-subset(list1,Case==case[j])
+    if(nrow(tmp)>0){
+    HTML.title(paste(paste(code,collapse=","),"codes within",case[j]), HR=1)
+  for(i in 1:nrow(tmp)){
+    HTML.title(paste("file:",tmp$filename[i]," code:",tmp$code[i]), HR=2)
+   HTMLhr()
+    R2HTML::HTML(tmp$text[i])
+
+  }
+    }
+  }
+
+  HTMLStop()
+
+  }
+
+  if(output=="df"){
+
+    return(list1)
+  }
+  if(!is.null(connection)){
+    dbDisconnect(con)
+  }
+}
